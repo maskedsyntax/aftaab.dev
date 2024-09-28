69863372fd28a72d3e995067096f278c4738c67c
aftaab25 <aftaab2507@gmail.com>
2024-09-28 14:15:02 +0530

    Modify skills from checkmarks to badges

--- src/common/SkillList.jsx
+++ src/common/SkillList.jsx
@@ -3,7 +3,7 @@ import styles from '../sections/Skills/SkillsStyles.module.css';
 function SkillList({src, skill}) {
   return (
     <span className={styles.badge}>
-      <img src={src} alt="Checkmark icon"/>
+      {/*<img src={src} alt="Checkmark icon"/>*/}
       <p className="badge-text">{skill}</p>
     </span>
   );
