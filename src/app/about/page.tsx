77c03199530aa2b4dc1c537abd53e0524db79d3a
Aftaab Siddiqui <aftaab2507@gmail.com>
2024-11-30 17:33:29 +0530

    Update About page by adding work experiences

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 import {PageTransition} from "@/components/page-transition";
 import type {Metadata} from "next";
 import {Heart} from "lucide-react";
+import {ExperienceSection} from "@/components/experience-section";
+import {AboutSection} from "@/components/about-section";
 
 export const metadata: Metadata = {
   title: 'About | Aftaab Siddiqui',
@@ -18,21 +20,24 @@ export default function AboutPage() {
       <div className="min-h-screen bg-background">
         <main className="container mx-auto px-6 py-8 pt-16 max-w-4xl">
           <h1 className="text-3xl font-bold mb-8 text-center">About Me</h1>
-          <section className="mb-12">
-            <h2 className="text-2xl font-bold mb-4 relative inline-block">
-              About
-              <div className="absolute bottom-0 left-0 w-full h-1 bg-red-500 -mb-1"></div>
-            </h2>
-            <p className="text-muted-foreground leading-relaxed">
-              Since childhood, I have been passionate about computers, technology, and gaming. While
-              gaming sparked my interest in game development, I ultimately found my true passion in
-              programming through various <span className="font-medium text-foreground">projects</span>.
-              I particularly enjoy <span className="font-medium text-foreground">competitive programming</span> and
-              tackling complex problems. I love learning new technologies and building websites with the
-              latest tech stacks. Additionally, I take pride in my typing skills, often reaching speeds of over
-              200 words per minute.
-            </p>
-          </section>
+          {/*<section className="mb-12">*/}
+          {/*  /!*<h2 className="text-2xl font-bold mb-4 relative inline-block">*!/*/}
+          {/*  /!*  My Journey in Technology*!/*/}
+          {/*  /!*  <div className="absolute bottom-0 left-0 w-full h-1 bg-red-500 -mb-1"></div>*!/*/}
+          {/*  /!*</h2>*!/*/}
+          {/*  <p className="text-muted-foreground leading-relaxed">*/}
+          {/*    Since childhood, I have been passionate about computers, technology, and gaming. While*/}
+          {/*    gaming sparked my interest in game development, I ultimately found my true passion in*/}
+          {/*    programming through various <span className="font-medium text-foreground">projects</span>.*/}
+          {/*    I particularly enjoy <span className="font-medium text-foreground">competitive programming</span> and*/}
+          {/*    tackling complex problems. I love learning new technologies and building websites with the*/}
+          {/*    latest tech stacks. Additionally, I take pride in my typing skills, often reaching speeds of over*/}
+          {/*    200 words per minute.*/}
+          {/*  </p>*/}
+          {/*  <hr className="my-4"/>*/}
+          {/*</section>*/}
+          <AboutSection/>
+          <ExperienceSection/>
         </main>
         <footer className="mt-20 pb-24 text-center">
           <div className="flex items-center justify-center mb-2">
