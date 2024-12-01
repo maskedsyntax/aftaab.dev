77b3403c32e5c519de1bb52f8b6892c26798b1ef
Aftaab Siddiqui <aftaab2507@gmail.com>
2024-12-01 19:20:30 +0530

    Remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,14 @@
 "use client";
 
 import "./globals.css";
-import {Fira_Sans, Inter, JetBrains_Mono, Montserrat, Roboto} from 'next/font/google'
+import {Fira_Sans, Inter} from 'next/font/google'
 import {ThemeProvider} from "@/components/themes";
 import Navbar from "@/components/navbar";
 import {AnimatePresence} from "framer-motion";
 import React from "react";
 import {ThemeWrapper} from "@/components/theme-wrapper";
 
-// Example for importing multiple fonts
-const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '700'], // Use desired weights
-  variable: '--font-roboto', // Custom CSS variable for this font
-});
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '600'],
-  variable: '--font-montserrat',
-});
-
-const jetBrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  // weight: ['400', '600'],
-  // variable: '--font-montserrat',
-});
 const firaSans = Fira_Sans({
   subsets: ['latin'],
   weight: ['400', '600'],
